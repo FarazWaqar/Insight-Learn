033c2658975ee54e542e64138f5e37f62b1fe703
Raveeha Choudhery <raveehachoudhry@gmail.com>
2024-12-08 19:30:30 +0500

    Never Give up...

--- app/src/main/java/com/example/insightlearn/Therapy_Activity.kt
+++ app/src/main/java/com/example/insightlearn/Therapy_Activity.kt
@@ -23,7 +23,7 @@ class TherapyActivity : AppCompatActivity() {
         }
 
         dysgraphiaButton.setOnClickListener {
-            val intent = Intent(this, graphtherapy::class.java)
+            val intent = Intent(this, graphtherapyactivity::class.java)
             startActivity(intent)
         }
 
