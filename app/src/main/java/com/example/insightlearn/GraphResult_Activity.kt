a703bf1dba5eb587df37bd58f00ea744792100b3
Raveeha Choudhery <raveehachoudhry@gmail.com>
2024-12-08 17:33:27 +0500

    Never Give up...

--- app/src/main/java/com/example/insightlearn/GraphResult_Activity.kt
+++ app/src/main/java/com/example/insightlearn/GraphResult_Activity.kt
@@ -1,12 +1,15 @@
 package com.example.insightlearn
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class graphActivityResult : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.graphresult_screen)
@@ -18,12 +21,30 @@ class graphActivityResult : AppCompatActivity() {
         val resultTextView = findViewById<TextView>(R.id.resultText)
         resultTextView.text = results
 
-        // Back Button to Go to Main Activity
+        // Back Button to Go to GraphDetection_Activity
         val backButton = findViewById<Button>(R.id.backButtonResult)
         backButton.setOnClickListener {
             val intent = Intent(this, GraphDetection_Activity::class.java)
             startActivity(intent)
             finish()
         }
+
+        // Home Button Click Listener
+        val homeButton = findViewById<ImageButton>(R.id.homeButton)
+        homeButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        /*
+        // Settings Button Click Listener
+        val settingsButton = findViewById<ImageButton>(R.id.settingsButton)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+         */
     }
 }
